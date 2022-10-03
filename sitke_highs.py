e324c67583d8a2412bc10e26c2329a2870d1116d
Janice-M <janiceink001@gmail.com>
2022-10-03 14:30:13 +0300

    opening and reading a csv file

--- sitke_highs.py
+++ sitke_highs.py
@@ -1,6 +1,18 @@
 import csv
+
+
+
+
 filename='data/simple.csv'
+
+def read_in_chunks (file_object, chunksize=100):
+    while True:
+        data= file_object.read(chunksize)
+        if not data:
+            break
+        yield data
+
+
 with open(filename) as f:
-    reader = csv.reader(f)
-    header_row= next(reader)
-    print(header_row)
+    for piece in read_in_chunks(f):
+        print(piece)
